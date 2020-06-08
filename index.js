d7cee1143330388b2fc55fdfeaac3a9d7e6925b8
Joshua Zhang <toruta39@gmail.com>
2020-06-08 17:17:08 +0900

    fix broken ref

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { getChangelist } = require('./lib/git')
 const { findPreviousRelease, findCurrentRelease } = require('./lib/releases')
 
 ;(async () => {
