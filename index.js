ac9a0a334a843a4e8836f7a62b1ffbc07440025e
Joshua Zhang <toruta39@gmail.com>
2020-06-08 11:51:01 +0900

    test git wrapper

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { findPreviousRelease, findCurrentRelease } = require('./lib/releases')
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput('time', time);
+
+
+  // get the reference to previous release
+  const previous = await findPreviousRelease()
+  // get the reference to current commit
+  const current = await findCurrentRelease()
+
+  // get changelist
+  const changelist = ''
+
+  // get watchlist input
+  const watchlist = core.getInput('watchlist')
+  // set changelist output
+  core.setOutput('changelist', 'TODO')
+  // grep the changelist if any change occurred on watchlist
+  // if so, set hit as true
+  core.setOuput('hit', true)
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
